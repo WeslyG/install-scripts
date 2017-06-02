cd0b69f943eebd93bbf35e1e84d7ff1b26ed47d2
WESLYG\WeslyG <v.lila@hostco.ru>
2017-06-02 22:52:56 +0500

    Доработал с переменными окружения. Теперь все распоковывается как нужно Модули автономны Протестированна работа JDK и maven Все отлично ставится, работает с входа следующей сессии

--- maven.sh
+++ maven.sh
@@ -7,14 +7,12 @@ sudo tar xzf apache-maven-${version}-bin.tar.gz -C /usr/local
 cd /usr/local
 rm -rf maven
 sudo ln -s apache-maven-${version} maven
-rm -rf /etc/profile.d/maven.sh
-echo "#!/bin/bash" >> /etc/profile.d/maven.sh
-echo "export M2_HOME=/usr/local/maven" >> /etc/profile.d/maven.sh
-source /etc/profile.d/maven.sh
-echo "export PATH=${M2_HOME}/bin:${PATH}" >> /etc/profile.d/maven.sh
+# rm -rf /etc/profile.d/maven.sh
+echo 'export M2_HOME=/usr/local/maven' >> ~/.bashrc
+echo 'export PATH=$M2_HOME/bin:$PATH' >> ~/.bashrc
 cd /tmp/
 rm -rf apache-maven-${version}-bin.tar.*
-source /etc/profile.d/maven.sh
+source ~/.bashrc
 echo "Все готово, выйдите и войдите в профиль снова, что бы все работало!"
 #Выйти и зайти
 
