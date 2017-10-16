fbc371dcc11e12016b75b40537380db3be0fc264
Lila Vladimir <v.lila@hostco.ru>
2017-10-16 13:51:25 +0500

    Update jdkInstall.sh

--- jdkInstall.sh
+++ jdkInstall.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 cd /tmp
-curl -o jdk-8u131-linux-x64.rpm https://cloud.hostco.ru/s/0qYpOfwjP5U24fK/download
-sudo yum localinstall jdk-8u131-linux-x64.rpm -y
-echo 'export JAVA_HOME=/usr/java/jdk1.8.0_131/jre' >> ~/.bashrc
-echo 'export PATH=/usr/java/jdk1.8.0_131/jre/bin:$PATH' >> ~/.bashrc
+name='jdk-8u144-linux-x64.rpm'
+versin=144
+curl -o ${name} https://cloud.hostco.ru/s/oYe2LHEIklTYrf6/download
+sudo yum localinstall ${name} -y
+echo "export JAVA_HOME=/usr/java/jdk1.8.0_${versin}/jre" >> ~/.bashrc
+echo "export PATH=/usr/java/jdk1.8.0_${versin}/jre/bin:\$PATH" >> ~/.bashrc
 source ~/.bashrc
-rm -rf jdk-8u131-linux-x64.rpm
+rm -rf ${name}
